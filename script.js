cb5b6dd84a567342b3779df0dafba75477951719
Ainan-code <ainanhassan123@gmail.com>
2022-06-11 09:40:15 +0300

    rendered the array content on the page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const Gameboard = (() => {
-    const gameboard = [];
+    const gameboard = ["X", "O", "X", "0", "X", "O", "X", "0", "X"];
     
+    function renderGameboard() {
+        let board = document.getElementById('gameboard');
+        
 
-})
+        for (i = 0; i < gameboard.length; i++) {
+            let div = document.createElement('div');
+            div.setAttribute('id', i)
+            div.className = 'box';
+            div.textContent = gameboard[i]
+            board.appendChild(div)
+        }
+
+    } 
+
+    return {
+        renderGameboard,
+        
+
+    }
+})();
+
+Gameboard.renderGameboard();
