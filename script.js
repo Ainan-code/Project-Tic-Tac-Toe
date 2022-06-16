7c5431e195511b4f961d8ac2c33722befa484828
Ainan-code <ainanhassan123@gmail.com>
2022-06-16 21:24:03 +0300

    display controller done in script js

--- script.js
+++ script.js
@@ -1,25 +1,101 @@
-const Gameboard = (() => {
-    const gameboard = ["X", "O", "X", "0", "X", "O", "X", "0", "X"];
-    
-    function renderGameboard() {
-        let board = document.getElementById('gameboard');
-        
 
-        for (i = 0; i < gameboard.length; i++) {
-            let div = document.createElement('div');
-            div.setAttribute('id', i)
-            div.className = 'box';
-            div.textContent = gameboard[i]
-            board.appendChild(div)
-        }
+ const board = document.querySelector('.gameboard')
+ const cells = document.querySelectorAll('[data-cellid]');
+
+
+
+const Gameboard = (() => {
+    const gameboard = [];
+    
+   
+
+   
+     
+   
+    
+    const renderGameboard = () => {
+        
+      for(let i=0; i < cells.length; i++) {
+         cells[i].setAttribute('data-cellid', i)
+        cells[i].textContent = gameboard[i]
+      
+        
+      }
+
 
     } 
 
+    const setCell = (index, marker)  => {
+      gameboard[index] = marker
+    }
+
+    
+
     return {
         renderGameboard,
+        setCell,
+        gameboard,
         
 
     }
 })();
 
 Gameboard.renderGameboard();
+
+
+
+const playerFactory = (name, marker) => {
+  
+   getname = () => name
+   getmarker = () => marker
+
+  
+  return {
+   
+    name, marker
+  }
+
+
+
+};
+
+const player1 = playerFactory('player1', "X");
+
+const player2 = playerFactory('player2', "0");
+
+console.log({player1, player2})
+
+
+const displayController = (() => {
+  
+   const  display = () => {
+
+     cells.forEach((element) => {
+       element.addEventListener('click', () => {
+
+
+         element.textContent = `${player2.marker}`;
+         Gameboard.gameboard.push(player2.marker);
+
+       });
+     });
+
+   }; 
+   
+   return {
+    display
+   };
+
+})();   
+     
+    
+  displayController.display();
+  
+  
+ 
+
+  
+
+
+
+
